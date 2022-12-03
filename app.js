05462bb1169e2a56eaddc9a057dede2c329fc9a0
Antonio Carlos <acdona@hotmail.com>
2022-12-03 15:44:07 -0300

    adicionando valores no to-do

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+const formAddTodo = document.querySelector('.form-add-todo')
+const todosContainer = document.querySelector('.todos-container')
 
+const addTodo = inputValue => {
+	if (inputValue.length) {
+		todosContainer.innerHTML += `
+		<li class="list-group-item d-flex justify-content-between align-items-center" data-todo="${inputValue}">
+		  <span>${inputValue}</span>
+		  <i class="far fa-trash-alt" data-trash="${inputValue}"></i>
+		</li>
+		`
+	}
+}
+
+formAddTodo.addEventListener('submit', event => {
+	event.preventDefault()
+	const inputValue = event.target.add.value.trim()
+	addTodo(inputValue)
+	event.target.reset()
+})
